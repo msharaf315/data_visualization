1a47da619f4dd6aa795afdfe2c6d3586d2f9dd2e
Muhammad Sharaf <msherf98@gmail.com>
2025-07-15 21:43:33 +0200

    feat added response json correctly

--- task_one/dashboard.js
+++ task_one/dashboard.js
@@ -17,7 +17,7 @@ let lineChart;
 let columns, rows;
 let filters = {};
 
-function initLineChart() {
+function initLineChart(data) {
   // set the dimensions and margins of the graph
   const margin = { top: 10, right: 30, bottom: 30, left: 60 },
     width = 460 - margin.left - margin.right,
@@ -32,80 +32,69 @@ function initLineChart() {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  //Read the data
-  d3.csv(
-    "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+  // Add X axis --> it is a date format
+  const x = d3
+    .scaleTime()
+    .domain(
+      d3.extent(data, function (d) {
+        return d.year;
+      })
+    )
+    .range([0, width]);
+  lineChart
+    .append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
 
-    // When reading the csv, I must format variables:
-    function (d) {
-      return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value };
-    }
-  ).then(
-    // Now I can use this dataset:
-    function (data) {
-      // Add X axis --> it is a date format
-      const x = d3
-        .scaleTime()
-        .domain(
-          d3.extent(data, function (d) {
-            return d.date;
-          })
-        )
-        .range([0, width]);
-      lineChart
-        .append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
+  // Add Y axis
+  const y = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(data, function (d) {
+        return +d.value;
+      }),
+    ])
+    .range([height, 0]);
+  lineChart.append("g").call(d3.axisLeft(y));
 
-      // Add Y axis
-      const y = d3
-        .scaleLinear()
-        .domain([
-          0,
-          d3.max(data, function (d) {
-            return +d.value;
-          }),
-        ])
-        .range([height, 0]);
-      lineChart.append("g").call(d3.axisLeft(y));
-
-      // Add the line
-      lineChart
-        .append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr(
-          "d",
-          d3
-            .line()
-            .x(function (d) {
-              return x(d.date);
-            })
-            .y(function (d) {
-              return y(d.value);
-            })
-        );
-    }
-  );
+  // Add the line
+  lineChart
+    .append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x(function (d) {
+          return x(d.date);
+        })
+        .y(function (d) {
+          return y(d.value);
+        })
+    );
 }
 
-function getData() {
-  fetch("http://127.0.0.1:8000/")
+async function getData() {
+  return await fetch("http://127.0.0.1:8000/")
     .then((response) => {
       return response.json(); // parse JSON data
     })
     .then((data) => {
-      console.log(data); // use the data
+      return data; // use the data
     })
     .catch((error) => {
       console.error("There was a problem with the fetch operation:", error);
     });
 }
 
-function initDashboard() {
+async function initDashboard() {
   console.log("init dashboard");
-  getData();
-  initLineChart();
+  data = await getData();
+  console.log(data);
+
+  initLineChart(data["line_chart_data"]);
 }
