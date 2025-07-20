cfec9dcf486689133b125cb5c86ba8f85948013e
Muhammad Sharaf <msherf98@gmail.com>
2025-07-20 15:57:59 +0200

    feat pictorial chart placement done with SVG

--- task_one/dashboard.js
+++ task_one/dashboard.js
@@ -243,6 +243,12 @@ function drawPieChart(data) {
 }
 
 function initPictorialChart(data) {
+  // define color scheme for causes
+  const causes = [...new Set(data.map((d) => d.cause))];
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(causes)
+    .range(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728"]);
   const margin = { top: 10, right: 30, bottom: 30, left: 70 },
     width = 700 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
@@ -267,12 +273,12 @@ function initPictorialChart(data) {
     .call(yAxis);
 
   // X axis
-  var x = d3.scaleLinear().domain([0, 10]).range([0, width]);
+  var xScale = d3.scaleLinear().domain([0, 10]).range([0, width]);
 
   svg
     .append("g")
     .attr("transform", `translate(${margin.left},` + height + ")")
-    .call(d3.axisBottom(x));
+    .call(d3.axisBottom(xScale));
 
   // --- 5. TOOLTIP ---
   const tooltip = d3.select(".tooltip");
@@ -290,26 +296,35 @@ function initPictorialChart(data) {
     tooltip.style("opacity", 0);
   };
 
+  const svg_string = `
+<g> <path id="_x36__3_" d="M173.7,171.4c-2.1,0.8-3.7,2.9-3.7,5.2l-0.2,2.3c-1,10.6-19.1,18.5-41.8,18.5s-40.8-7.9-41.8-18.5l-0.2-2.1   c-0.2-2.3-1.5-4.4-3.9-5.2c0,0,0,0-0.2,0c-4.4-1.7-9.1,1.5-8.7,6.2l1.3,19.3c0.8,10,19.3,25.6,53.4,25.6   c33.1,0,52.6-15.6,53.4-25.6l1.3-19.5C182.9,173,178.3,169.7,173.7,171.4C174,171.4,174,171.4,173.7,171.4z"/>
+  <path id="_x35__3_" d="M191.2,56.4c-10.6-16.2-30.2-27-61.8-27c-0.6,0-1,0-1.3,0c-0.6,0-0.8,0-1.3,0c-31.6,0-51.2,10.8-61.8,27   c-9.4,14.6-11.6,33.3-8.7,52.8c4,26.4,4,47.2,33.7,55.7l1,10.2c0.6,5.2,13.3,12.9,35.6,12.9c0.6,0,1.3,0,1.7,0c0.6,0,1,0,1.7,0   c22.2,0,35.3-7.7,35.6-12.9l1-10.2c29.1-8.5,29.1-29.3,33.1-55.7C202.9,90,200.6,71.1,191.2,56.4z M117,125.6   c0,8.3-6.7,14.8-14.8,14.8H89.1c-5.4,0-10-4.4-10-10v-9.8c0-10,8.3-18.3,18.3-18.3h9.8c5.4,0,10,4.4,10,10v13.3   C117.2,125.6,117,125.6,117,125.6z M134.2,161.2h-12.5c-3.3,0-6-3.1-4.6-6.2l6.2-14.1c1.5-3.7,7.5-3.7,9.1,0l6.2,14.1   C140.1,158,137.8,161.2,134.2,161.2z M177.1,130.6c0,5.4-4.4,10-10,10H154c-8.3,0-14.8-6.7-14.8-14.8v-13.3c0-5.4,4.4-10,10-10h9.8   c10,0,18.3,8.3,18.3,18.3v9.8H177.1z"/>
+  <path id="_x34__10_" d="M197.5,43.7c1.5,2.1,3.1,3.9,4.4,6c3.7,5.4,6.2,11.4,8.3,17.5l19.5-19.5c5.6,5.6,15.4,4.8,19.8-2.3   c2.5-3.9,2.5-9.2,0-13.1c-4.4-7.1-14.1-7.9-19.8-2.3c5.6-5.6,4.8-15.4-2.3-19.8c-3.9-2.5-9.2-2.5-13.1,0   c-7.1,4.4-7.9,14.1-2.3,19.8L197.5,43.7z"/>
+  <path id="_x33__7_" d="M229.7,208.2L195,173.8c0.2,1.5,0.6,3.1,0.2,4.8l-1.3,19.5c-0.2,2.9-1,5.4-2.3,8.3l20,20   c-4.8,4.8-4.8,13.1,0,17.9c4.8,4.8,13.1,4.8,17.9,0c4.8-4.8,4.8-13.1,0-17.9c4.8,4.8,13.1,4.8,17.9,0c4.8-4.8,4.8-13.1,0-17.9   C242.4,203.2,234.5,203.2,229.7,208.2z"/>
+  <path id="_x34__11_" d="M58.5,43.7c-1.5,2.1-3.1,3.9-4.4,6c-3.7,5.4-6.2,11.4-8.3,17.5L26.3,47.8c-5.6,5.6-15.4,4.8-19.8-2.3   C4,41.6,4,36.3,6.5,32.4c4.4-7.1,14.1-7.9,19.8-2.3c-5.6-5.6-4.8-15.4,2.3-19.8c3.9-2.5,9.2-2.5,13.1,0c7.1,4.4,7.9,14.1,2.3,19.8   L58.5,43.7z"/>
+  <path id="_x33__8_" d="M26.3,208.2L61,173.6c-0.2,1.5-0.6,3.1-0.2,4.8l1.3,19.5c0.2,2.9,1,5.4,2.3,8.3l-20,20   c4.8,4.8,4.8,13.1,0,17.9c-4.8,4.8-13.1,4.8-17.9,0s-4.8-13.1,0-17.9c-4.8,4.8-13.1,4.8-17.9,0s-4.8-13.1,0-17.9   C13.6,203.2,21.5,203.2,26.3,208.2z"/>
+</g>`;
+
   // --- 6. DRAWING THE ICONS (PICTORIAL CHART) ---
   svg
     .append("g")
-    .selectAll("icon")
+    .attr("transform", `translate(${margin.left}, 10)`)
+    .selectAll("g.icon-group") // Select a class to avoid conflicts
     .data(data)
     .enter()
-    .append("circle")
-    .attr("transform", `translate( ${margin.left},` + "0)")
-    .attr("cx", function (d) {
-      console.log(d);
-
-      return x(d["x_location"]);
-    })
-    .attr("cy", function (d) {
-      return yScale(d["category"]);
-    })
-    .attr("r", 10)
+    .append("g") // Append a 'g' element for each icon
+    .attr("class", "icon-group") // Add a class for styling/selection
+    .html(svg_string) // Insert the SVG content
     .style("cursor", "pointer")
-    .style("fill", function (d) {
-      "blue";
+    .attr("transform", (d) => {
+      const translateX = xScale(d.x_location); // Position based on x_location, centered
+      const translateY = yScale(d.category); // Position based on x_location, centered
+
+      return `translate(${translateX}, ${translateY}) scale(0.1)`;
+    })
+
+    .attr("fill", (d) => {
+      return colorScale(d.cause);
     });
 }
 
